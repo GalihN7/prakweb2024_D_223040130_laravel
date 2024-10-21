a93078cd02fc6d3880e85cc28c30a32f2d690e85
GalihN7 <galihnurhuda07@gmail.com>
2024-10-21 12:33:42 +0700

    Templating Engine
    Video Belajar Laravel 11 | 4. Blade Templating Engine

--- laravel11/routes/web.php
+++ laravel11/routes/web.php
@@ -3,5 +3,23 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about', ['nama' =>'Galih Nurhuda']);
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+// Buat 2 rute baru
+// 1. /blog
+// 2 buah artikel, judul dan isi
+// 2. /contact
+// email / social media 
